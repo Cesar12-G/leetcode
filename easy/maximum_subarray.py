b18706bb899d14f22ae3593787db2e261bb21255
Cesar Rodriguez <rodriguezcesar1125@gmail.com>
2020-07-19 20:22:34 -0700

    Maximum subarray not completed

---
+++ easy/maximum_subarray.py
@@ -0,0 +1,12 @@
+"""
+53. Maximum Subarray
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+"""
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
